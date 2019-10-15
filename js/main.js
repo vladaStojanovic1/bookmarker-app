c6671d27b588eb73679c65ff9181ad3114af8a5c
1vlada1 <1vladastojanovic1@gmail.com>
2019-10-15 18:49:07 +0200

    Fetch bookmark and show on page

--- js/main.js
+++ js/main.js
@@ -29,9 +29,33 @@ function saveBookmark(e) {
         // Re-set back to LocalStorage
         localStorage.setItem('bookmars', JSON.stringify(bookmars));
     }
-
-
     e.preventDefault();
 }
 
 
+
+// Fetch bookmars
+function fetchBookmars() {
+    // Get bookmars from localStorage
+    let bookmars = JSON.parse(localStorage.getItem('bookmars'));
+
+    // Get output id
+    let bookmarksResults = document.getElementById('bookmarksResults');
+    // Build output
+    // bookmarksResults.innerText = ''
+    bookmars.forEach((bookmark) => {
+        let name = bookmark.name;
+        let url = bookmark.url;
+
+        bookmarksResults.innerHTML += `
+        <div class='card card-body bg-light text-center w-50 m-auto'>
+            <h3>${name}
+                <a href=${url} class='btn btn-primary' target='_blank'> Visit</a>
+                <a href='#'  class='btn btn-danger'>X</a>
+            </h3>
+        </div>
+        `
+
+    })
+
+}
